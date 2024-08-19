6d7e1eedb784fdb79c5a2a41f8bef342640bd3f9
Nendy Rafi <nendyrafi@gmail.com>
2024-08-19 22:55:43 +0700

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <form className="p-1 text-gray-50 bg-gray-900 h-screen">
+      <h1 className="font-bold text-2xl">ToDo List</h1>
+    </form>
+  )
+}
+
+export default App
